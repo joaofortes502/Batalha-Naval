6a46735aac4caf0bf810f5d6a5c94047cedf3e8f
João Arthur Fortes <joaofortes502@gmail.com>
2024-09-27 15:29:55 -0300

    estruta do que vamos fazer

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,6 +6,34 @@ public class Main {
 
         int tipoJogo = modoDeJogo.definirModoDeJogo();
 
+
+        if (tipoJogo==0){
+            //jogador contra jogador
+
+            //seleção dos barcos --> Manual ou Automatica?
+
+            //Manual{
+            //}else Automatica{
+            //}
+
+            //inicia jogo
+            //ataca -> verifica situacao -> ataca -> verifica situacao
+
+        }else{
+            //jogador contra computador
+
+            //seleção dos barcos --> Manual ou Automatica?
+
+            //Manual{
+            //}else Automatica{
+            //}
+
+            //inica jogo
+            //ataca -> verifica situacao -> ataca -> verifica situacao
+
+        }
+
+
         //tabuleiro1.printarTabuleiro();
         final RetornoHandler resposta = tabuleiro1.adicionarNavio("B7", 5, false);
         if (!resposta.isSucesso()) {
